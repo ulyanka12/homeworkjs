8f3896dad34133cfcac85a4da06b3d41062c1453
Uliana Boiarchuk <uliana.boiarchuk@gmail.com>
2018-01-26 18:20:20 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,28 +1,33 @@
-const a  = parseInt(prompt(`Enter a`));
+const a = parseInt(prompt(`Enter a`));
 const b = parseInt(prompt("Enter b"));
 const c = parseInt(prompt("Enter c"));
 
 function discriminant(a, b, c) {
- 
-    let discr =(b*b) + (4 * a * c);
-    return discr;
-}
- const d = discriminant;
- 
- function quadraticEq (a, b, c){
-    if (d > 0) {
-        const twoA=parseInt(2*a);
-        let x1 = (-b + Math.sqrt(d)) / twoA;
-       let x2 = -b / twoA + Math.sqrt(d) / twoA;
-        return x1, x2;
-    }
-     else  {
-        const twoA=parseInt(2*a);
-        const x3 = -b / twoA;
-        return x3;
-    }
-   
- }    
 
-const result = quadraticEq (a, b, c);
+ let discr = (b * b) + (4 * a * c);
+ return discr;
+}
+const d = discriminant;
+
+function quadraticEq(a, b, c) {
+ if (d > 0) {
+  const twoA = parseInt(2 * a);
+  let x1 = (-b + Math.sqrt(d)) / twoA;
+  let x2 = -b / twoA + Math.sqrt(d) / twoA;
+  return x1, x2;
+ }
+ else if (d===0) {
+  const twoA = parseInt(2 * a);
+  const x3 = -b / twoA;
+  return x3;
+ }
+ else{
+  document.write("No answer")
+ }
+}
+
+const result = quadraticEq(a, b, c);
 document.write(result);
+
+
+
